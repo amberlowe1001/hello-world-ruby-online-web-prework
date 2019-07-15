dd5b85209533a036734446e0e4c0f107efdce915
lite-post-0822 <amberlowe40@gmail.com>
2019-07-15 06:14:57 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,10 @@
-puts "Hello_World!"
+def Hello_World (introduction)
+  introduction - "Hello_World"
+  puts Hello World
+ 
+ end
+
+def "Hello_World"(""Hello_World"")
+	puts = "Hello World!"
+end
+
